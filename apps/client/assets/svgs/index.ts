3b71675084e348151afcd4f6575897812b81ec5b
chigomuh <tngusrldnjs1@gmail.com>
2023-02-03 19:59:52 +0900

    feat(client): base nav를 만듭니다.

---
+++ apps/client/assets/svgs/index.ts
@@ -0,0 +1,7 @@
+import chevronLeft from './chevron-left.svg';
+
+const icons = {
+  chevronLeft,
+};
+
+export default icons;
--- apps/client/components/Layout/AppLayout/Nav/index.tsx
+++ apps/client/components/Layout/AppLayout/Nav/index.tsx
@@ -1,19 +1,73 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Center, Flex, Spacer, Text, Box } from '@chakra-ui/react';
 import { css, useTheme } from '@emotion/react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import icons from '~/assets/svgs';
 
 const Nav = () => {
-  const { typography } = useTheme();
+  const { pathname } = useRouter();
+  const { isDetail } = getRouterState(pathname);
+  const { typography, colors } = useTheme();
+
   return (
-    <Flex>
-      <Text
-        css={css`
-          ${typography.headline4}
-        `}
-      >
-        GD News
-      </Text>
-    </Flex>
+    <header
+      css={css`
+        width: 100%;
+        height: 100%;
+        padding: 1rem;
+        color: ${colors.white};
+        background-color: ${colors.primary};
+      `}
+    >
+      <nav>
+        <Flex>
+          {isDetail ? (
+            <Center>
+              <Image
+                src={icons.chevronLeft}
+                alt="chevron-left"
+                width={32}
+                height={32}
+              />
+            </Center>
+          ) : (
+            <Box
+              css={css`
+                width: 2rem;
+                height: 2rem;
+              `}
+            />
+          )}
+          <Spacer />
+          <Text
+            css={css`
+              ${typography.headline5}
+            `}
+          >
+            GD News
+          </Text>
+          <Spacer />
+          <Center>
+            <Text
+              css={css`
+                ${typography.subtitle1}
+              `}
+            >
+              로그인
+            </Text>
+          </Center>
+        </Flex>
+      </nav>
+    </header>
   );
+};
+
+const getRouterState = (pathname: string) => {
+  const detail = ['/news/[id]'];
+
+  return {
+    isDetail: detail.includes(pathname),
+  };
 };
 
 export default Nav;
