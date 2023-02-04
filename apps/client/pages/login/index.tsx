f62fc24e58604e4e63991a20b21749374a54317c
chigomuh <tngusrldnjs1@gmail.com>
2023-02-04 15:32:57 +0900

    feat(client): 로그인 페이지 스타일을 구현합니다.

--- apps/client/pages/login/index.tsx
+++ apps/client/pages/login/index.tsx
@@ -1,5 +1,97 @@
+import { Button, Container, Heading, Stack, Text } from '@chakra-ui/react';
+import { useTheme, css } from '@emotion/react';
+
 const login = () => {
-  return <div>로그인페이지</div>;
+  const { colors, typography } = useTheme();
+
+  return (
+    <Container
+      css={css`
+        padding: 1rem;
+        width: 100%;
+        height: 100%;
+        margin-top: 4rem;
+      `}
+    >
+      <Stack
+        css={css`
+          gap: 4rem;
+        `}
+      >
+        <Heading
+          css={css`
+            font-size: 1.5rem;
+            text-align: center;
+          `}
+        >
+          가입하면{' '}
+          <Text
+            css={css`
+              display: inline;
+              font-weight: bold;
+            `}
+          >
+            다양한 기능
+          </Text>
+          <br />
+          사용 가능
+        </Heading>
+        <Stack
+          css={css`
+            justify-content: center;
+            align-items: center;
+            gap: 0.5rem;
+          `}
+        >
+          <Button
+            css={css`
+              background-color: ${colors.kakao};
+              width: 20rem;
+              height: 3rem;
+              border-radius: 0.5rem;
+              &:before {
+                content: '3초안에 로그인하기';
+                position: absolute;
+                top: -3rem;
+                left: 50%;
+                padding: 0.5rem 1rem;
+                transform: translateX(-50%);
+                color: ${colors.black};
+                background-color: ${colors.white};
+                border: 1px solid ${colors.grayE8};
+                border-radius: 1.5rem;
+              }
+            `}
+          >
+            <Text
+              css={css`
+                ${typography.headline5}
+              `}
+            >
+              카카오로 계속하기
+            </Text>
+          </Button>
+          <Button
+            css={css`
+              background-color: ${colors.google};
+              width: 20rem;
+              height: 3rem;
+              border-radius: 0.5rem;
+            `}
+          >
+            <Text
+              css={css`
+                ${typography.headline5}
+                color: ${colors.white}
+              `}
+            >
+              구글로 계속하기
+            </Text>
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
+  );
 };
 
 export default login;
--- apps/client/styles/GlobalStyles.tsx
+++ apps/client/styles/GlobalStyles.tsx
@@ -9,6 +9,10 @@ const GlobalStyles = () => {
           padding: 0;
           font: inherit;
           color: inherit;
+          font-family: 'Pretendard Variable', Pretendard, -apple-system,
+            BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
+            'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
+            'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
         }
 
         *,
