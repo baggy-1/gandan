4cf8dabd8ff7e003790d340cc02b314b0819df0b
chigomuh <tngusrldnjs1@gmail.com>
2023-02-07 02:00:33 +0900

    chore(client): 홈에 테스트 로직을 삭제합니다

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,15 +1,5 @@
-import Cookies from 'js-cookie';
-import { token } from '~/constants';
-import api from '~/services/client/api';
-
 const Home = () => {
-  const onClick = () => {
-    api.post('/api/oauth/token', {
-      refreshToken: Cookies.get(token.refreshToken),
-    });
-  };
-
-  return <button onClick={onClick}>버튼</button>;
+  return <div>홈</div>;
 };
 
 export default Home;
