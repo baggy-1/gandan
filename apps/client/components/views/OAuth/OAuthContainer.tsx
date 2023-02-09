7008f66f976e07ab78f14fea12d7bb9d3949bdf9
chigomuh <tngusrldnjs1@gmail.com>
2023-02-09 17:21:33 +0900

    feat(client): oauth page loading 화면을 구성합니다

--- apps/client/components/views/OAuth/OAuthContainer.tsx
+++ apps/client/components/views/OAuth/OAuthContainer.tsx
@@ -1,5 +1,22 @@
+import { Center, Spinner } from '@chakra-ui/react';
+import { css } from '@emotion/react';
+
 const OAuthContainer = () => {
-  return <div>로딩</div>;
+  return (
+    <Center
+      css={css`
+        width: 100%;
+        height: 100vh;
+      `}
+    >
+      <Spinner
+        css={css`
+          width: 3rem;
+          height: 3rem;
+        `}
+      />
+    </Center>
+  );
 };
 
 export default OAuthContainer;
