195891cf2cc8fddee2c1dbffe10bf0bef0426c04
chigomuh <tngusrldnjs1@gmail.com>
2023-02-11 01:06:18 +0900

    fix(client): google oauth user-agent headers 추가

--- apps/client/services/server/user/apis.ts
+++ apps/client/services/server/user/apis.ts
@@ -14,6 +14,8 @@ export const getGoogleUser = (token: string) => {
     {
       headers: {
         Authorization: `Bearer ${token}`,
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
       },
     }
   );
