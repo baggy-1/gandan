a83b81f225656c999799c22e29f18e940c1d0fee
chigomuh <tngusrldnjs1@gmail.com>
2023-02-11 16:18:32 +0900

    fix(client): refetchwindowfocus 옵션을 수정합니다

--- apps/client/services/client/news/queries.ts
+++ apps/client/services/client/news/queries.ts
@@ -3,9 +3,13 @@ import queryKeys from '../querykeys';
 import { getNews, getNewsById } from './apis';
 
 export const useQueryNews = () => {
-  return useSuspensedQuery(queryKeys.news, getNews);
+  return useSuspensedQuery(queryKeys.news, getNews, {
+    refetchOnWindowFocus: false,
+  });
 };
 
 export const useQueryNewsById = (id: string) => {
-  return useSuspensedQuery(queryKeys.newsById(id), () => getNewsById(id));
+  return useSuspensedQuery(queryKeys.newsById(id), () => getNewsById(id), {
+    refetchOnWindowFocus: false,
+  });
 };
