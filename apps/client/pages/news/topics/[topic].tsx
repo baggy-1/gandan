bf5a7a593bbec7f79af766ef97b19970747497d0
chigomuh <tngusrldnjs1@gmail.com>
2023-02-25 15:44:18 +0900

    feat(client: topic page에 SSR을 도입합니다

--- apps/client/pages/news/topics/[topic].tsx
+++ apps/client/pages/news/topics/[topic].tsx
@@ -1,8 +1,12 @@
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 import TopicDetailViews from '@views/TopicDetail';
 import { topic as TOPIC } from '~/constants';
 import { OpenGraph } from '~/components/common';
 import { hasProperty } from '~/utils';
+import queryKeys from '~/services/client/querykeys';
+import { getNewsByTopic } from '~/services/client/news/apis';
 
 const Topic = () => {
   const router = useRouter();
@@ -24,3 +28,39 @@ const Topic = () => {
 };
 
 export default Topic;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  if (!params || !params.topic || typeof params.topic !== 'string') {
+    return {
+      props: {},
+    };
+  }
+
+  const { topic } = params;
+
+  if (!isTopicKeyword(topic)) {
+    return {
+      props: {},
+    };
+  }
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    queryKeys.newsByTopic(topic),
+    () => getNewsByTopic(topic),
+    {
+      staleTime: 1000 * 60 * 5,
+    }
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+const isTopicKeyword = (topic: string): topic is Topic => {
+  return Object.keys(TOPIC).includes(topic);
+};
